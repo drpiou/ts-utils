148501199447bac27f54d3d18811786aa6dc974f
Alex <dr.piou@gmail.com>
2022-08-06 00:55:53 +0200

    test with vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 import { resolve } from 'path'
 import { defineConfig } from 'vite'
+import dts from 'vite-plugin-dts'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [
+    dts({
+      insertTypesEntry: true,
+    }),
+  ],
   build: {
     lib: {
       entry: resolve(__dirname, 'lib/index.ts'),
